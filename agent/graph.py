b6dc52e6c2776575f65a14306efb6a84057644d4
Shubham <sharmashubham6822@gmail.com>
2025-09-04 11:39:10 +0530

    Implements basic LangGraph agent

--- agent/graph.py
+++ agent/graph.py
@@ -1,5 +1,6 @@
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
+from langgraph.graph import StateGraph
 from prompt import *
 from states import *
 
@@ -8,7 +9,20 @@ load_dotenv()
 llm = ChatGroq(model="openai/gpt-oss-120b")
 
 user_prompt = "create a simple calculator web application"
+def planner_agent(state:dict) -> dict:
+    user_prompt= state["user_prompt"]
+    # Getting planning response
+    resp = llm.with_structured_output(Plan).invoke(planner_prompt(user_prompt))
+    return {"plan": resp}
 
-resp = llm.with_structured_output(Plan).invoke(planner_prompt(user_prompt))
 
-print(resp)
+
+graph = StateGraph(dict)
+graph.add_node("planner",planner_agent)
+graph.set_entry_point("planner")
+
+agent = graph.compile()
+
+result = agent.invoke({"user_prompt": user_prompt})
+print(result)
+
