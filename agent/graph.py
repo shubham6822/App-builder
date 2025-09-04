79b27b33808c77fa435a3c15de933d7722e2441e
Shubham <sharmashubham6822@gmail.com>
2025-09-04 10:32:29 +0530

    llm response with structured

--- agent/graph.py
+++ agent/graph.py
@@ -1,11 +1,18 @@
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
+from pydantic import BaseModel
 
 load_dotenv()
 
 llm = ChatGroq(model="openai/gpt-oss-120b")
 
-res = llm.invoke("who created python answer in ! sentence")
+class Schema(BaseModel):
+    price: float
+    eps: float
 
-print(res.content)
+res = llm.with_structured_output(Schema).invoke("Extract Price and EPS from this report:"
+"NVIDIA reported quarterly EPS of 2.3 and"
+"Their current price is $100.")
+
+print(res)
 
