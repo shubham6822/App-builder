3cda4ecf0642198081c63c98ec859e7ba6104d6d
Shubham <sharmashubham6822@gmail.com>
2025-09-04 12:04:00 +0530

    architecture agent

--- agent/graph.py
+++ agent/graph.py
@@ -9,16 +9,27 @@ load_dotenv()
 llm = ChatGroq(model="openai/gpt-oss-120b")
 
 user_prompt = "create a simple calculator web application"
+
 def planner_agent(state:dict) -> dict:
     user_prompt= state["user_prompt"]
     # Getting planning response
     resp = llm.with_structured_output(Plan).invoke(planner_prompt(user_prompt))
+    if resp is None:
+        raise ValueError("Planner failed")
     return {"plan": resp}
 
-
+def architecture_agent(state:dict) -> dict:
+    plan =  state["plan"]
+    resp = llm.with_structured_output(TaskPlan).invoke(architect_prompt(plan))
+    if resp is None:
+        raise ValueError("Architecture failed")
+    resp.plan = plan
+    return {"task_plan": resp}
 
 graph = StateGraph(dict)
 graph.add_node("planner",planner_agent)
+graph.add_node("architecture",architecture_agent)
+graph.add_edge("planner", "architecture")
 graph.set_entry_point("planner")
 
 agent = graph.compile()
--- agent/prompt.py
+++ agent/prompt.py
@@ -3,3 +3,21 @@ def planner_prompt(user_prompt:str) -> str:
     You are the PLANNER agent. Convert the user prompt into a COMPLETE engineering project plan
     User request: {user_prompt}
     """
+
+def architect_prompt(plan: str) -> str:
+    return f"""
+You are the ARCHITECT agent. Given this project plan, break it down into explicit engineering tasks.
+
+RULES:
+- For each FILE in the plan, create one or more IMPLEMENTATION TASKS.
+- In each task description:
+    * Specify exactly what to implement.
+    * Name the variables, functions, classes, and components to be defined.
+    * Mention how this task depends on or will be used by previous tasks.
+    * Include integration details: imports, expected function signatures, data flow.
+- Order tasks so that dependencies are implemented first.
+- Each step must be SELF-CONTAINED but also carry FORWARD the relevant context from earlier tasks.
+
+Project Plan:
+{plan}
+    """
